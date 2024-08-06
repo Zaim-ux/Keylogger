dc4e2599667c2c977a18541f551055cf6ca6af62
Zaim <Zaim.Abdin.2022@live.rhul.ac.uk>
2024-08-06 22:53:48 +0100

    Project initialisation

---
+++ src/test/java/com/zaim/keylogger/KeyloggingTests.java
@@ -0,0 +1,14 @@
+package com.zaim.keylogger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KeyloggingTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
